bee4ddd7433609d89df031841d2669349832e10a
Galdina <mari.galdina@gmail.com>
2020-06-03 06:41:22 -0700

    last minute change
    add readme.md

--- py/dataframecheck.py
+++ py/dataframecheck.py
@@ -1,5 +1,5 @@
 def sum_info(df):
-    print(f"Dataset Shape: {df.shape}")
+    print("Number of rows: {0}, number of columns: {1}".format(df.shape[0], df.shape[1]))
     sum_ = pd.DataFrame(df.dtypes, columns=['dtypes'])
     sum_ = sum_.reset_index()
     sum_['Name'] = sum_['index']
